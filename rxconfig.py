e7d2f79588e65324496f56af0312babd27005020
DiegoEx27 <diegoeyedpeas@gmail.com>
2024-01-20 21:55:13 -0300

    cambio_rconfig2

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,6 @@ import reflex as rx
 config = rx.Config(
     app_name="Reflex",
     db_url="mysql+mysqldb://root:@localhost/pruebas",
-    api_url="http://app.example.com:8000"
+    api_url="prueba2-reflex.vercel.app"
 )
 #mysql://scott:tiger@localhost/foo
